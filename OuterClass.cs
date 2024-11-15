e906107b66374dd27eaf8e5c22bdbc1232f601df
lxy <lxy0xabcdefg@qq.com>
2024-11-15 10:12:43 +0800

    Initial commit

---
+++ OuterClass.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI.SourceGenerators;
+
+namespace ReactiveGeneratorSample;
+
+public static partial class OuterClass
+{
+    public partial class InnerClass
+    {
+        [ReactiveCommand]
+        public void Test()
+        {
+        }
+    }
+    
+    private static void Main()
+    {
+        
+    }
+}
